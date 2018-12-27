558f027d06aa98fbbd748a710204c44203c00d5c
krishna_kumar14 <krishna_kumar14@dellteam.com>
2018-12-27 11:49:13 +0530

    Committing Angular 2 project

---
+++ Angular2/Part1/Part1/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: 'my-app',
+    //template: `<h1>Hello {{name}}</h1>`,
+    templateUrl: '/HtmlPage1.html',
+})
+
+export class AppComponent { name: string = "Angular 2."; }
+
+
+
+
+//import { Component } from '@angular/core';
+
+//@Component({
+//  selector: 'my-app',
+//  template: `<h1>Hello {{name}}</h1>`,
+//})
+//export class AppComponent  { name = 'Angular2.'; }
